f855b18fbd60ac4f26a25755ddf7a4f7c895cc18
Tarek Btw <tarekabdullah@outlook.de>
2025-06-04 01:49:08 +0200

    add DirectX 11 device overlay and ImGui integration with MinHook support

---
+++ PacketTracerQueryHook/device_overlay.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <d3d11.h>
+#include "kiero.h"
+#include "minhook/include/MinHook.h"
+
+
+typedef BOOL(__stdcall* wglSwapBuffers_t)(HDC hDc);
+extern wglSwapBuffers_t oSwapBuffers;  
+
+DWORD WINAPI MainThread(LPVOID);
